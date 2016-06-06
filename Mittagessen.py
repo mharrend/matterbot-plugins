d13f2f8ac32e12c18fda6540e20868ed38c3e075
root <root@ekplx89.physik.uni-karlsruhe.de>
2016-06-07 00:06:35 +0200

    Matterbot ownplugins added weather bot, fixed bug that Speiseplan is shown everybody

--- Mittagessen.py
+++ Mittagessen.py
@@ -1,3 +1,4 @@
+ # -*- coding: utf-8 -*-
 from mattermost_bot.bot import respond_to, listen_to
 import os
 import pymsgbox
@@ -13,11 +14,11 @@ def mittagessen_aufruf(message):
     getUserName = getpass.getuser()
     kommtMit = pymsgbox.confirm('Kommst Du mit zum Mittagessen', 'Teilnahme Mittagessen', ["Ja", "Vielleicht", 'Nein'])
     if kommtMit == "Ja":
-	message.reply('Ja, %s kommt mit zum Mittagessen' % getUserName)
+	message.send('Ja, %s kommt mit zum Mittagessen' % getUserName, '7iqsqeqaefdmdcy4nj7ccnwfnw')
     elif kommtMit == "Vielleicht":
-   	message.reply('Vielleicht kommt %s mit zum Mittagessen' % getUserName)
+   	message.send('Vielleicht kommt %s mit zum Mittagessen' % getUserName, '7iqsqeqaefdmdcy4nj7ccnwfnw')
     else:
-        message.reply('%s Kommt nicht mit zum Mittagessen' % getUserName)
+        message.send('%s Kommt nicht mit zum Mittagessen' % getUserName, '7iqsqeqaefdmdcy4nj7ccnwfnw')
 
 mittagessen_aufruf.__doc__ = "Zeigt Speiseplan an und macht Umfrage, wer zum Mittagessen mitkommt"
 
@@ -28,17 +29,20 @@ def mittagessen_um(message, uhrzeit):
     getUserName = getpass.getuser()
     kommtMit = pymsgbox.confirm('Kommst Du mit zum Mittagessen um %s' % uhrzeit , 'Teilnahme Mittagessen', ["Ja", "Vielleicht", 'Nein'])
     if kommtMit == "Ja":
-        message.reply('Ja, %s kommt mit zum Mittagessen' % getUserName)
+        message.send('Ja, %s kommt mit zum Mittagessen' % getUserName, '7iqsqeqaefdmdcy4nj7ccnwfnw')
     elif kommtMit == "Vielleicht":
-        message.reply('Vielleicht kommt %s mit zum Mittagessen' % getUserName)
+        message.send('Vielleicht kommt %s mit zum Mittagessen' % getUserName, '7iqsqeqaefdmdcy4nj7ccnwfnw')
     else:
-        message.reply('%s Kommt nicht mit zum Mittagessen' % getUserName)
+        message.send('%s Kommt nicht mit zum Mittagessen' % getUserName, '7iqsqeqaefdmdcy4nj7ccnwfnw')
 
 mittagessen_um.__doc__ = "Umfrage, wer zu einer bestimmten Zeit zum Mittagessen mitkommt"
 
 @respond_to('SpeiseplanAnzeigen')
 @listen_to('SpeiseplanAnzeigen')
 def mittagessen_speiseplan(message):
-    os.system('xdg-open "http://www.aserv.kit.edu/downloads/Speiseplan_deutsch.pdf"')
+    # Wichtig, damit nur der ausfuehrende User die Antwort angezeigt bekommt
+    if message.get_username() == getpass.getuser():
+	os.system('xdg-open "http://www.aserv.kit.edu/downloads/Speiseplan_deutsch.pdf"')
 
-mittagessen_speiseplan.__doc__ = "Zeigt Speiseplan an"
+mittagessen_speiseplan.__doc__ = u"Zeigt Speiseplan für einzelnen Benutzer an"
+
