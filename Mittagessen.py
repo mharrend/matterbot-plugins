4520c506ca2980b54cc3e14f0e963e6806821b48
root <root@ekplx89.physik.uni-karlsruhe.de>
2016-09-22 18:26:05 +0200

    Mittagessen.py: Updated indentation via autopep8

--- Mittagessen.py
+++ Mittagessen.py
@@ -1,4 +1,4 @@
- # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from mattermost_bot.bot import respond_to, listen_to
 import os
 import pymsgbox
@@ -6,19 +6,29 @@ import time
 import getpass
 import re
 
+
 @respond_to('MittagessenNun')
 @listen_to('MittagessenNun')
 def mittagessen_aufruf(message):
     os.system('xdg-open "http://www.aserv.kit.edu/downloads/Speiseplan_deutsch.pdf"')
     time.sleep(3)
     getUserName = getpass.getuser()
-    kommtMit = pymsgbox.confirm('Kommst Du mit zum Mittagessen', 'Teilnahme Mittagessen', ["Ja", "Vielleicht", 'Nein'])
+    kommtMit = pymsgbox.confirm(
+        'Kommst Du mit zum Mittagessen', 'Teilnahme Mittagessen', [
+            "Ja", "Vielleicht", 'Nein'])
     if kommtMit == "Ja":
-	message.send('Ja, %s kommt mit zum Mittagessen' % getUserName, '7iqsqeqaefdmdcy4nj7ccnwfnw')
+        message.send(
+            'Ja, %s kommt mit zum Mittagessen' %
+            getUserName,
+            '7iqsqeqaefdmdcy4nj7ccnwfnw')
     elif kommtMit == "Vielleicht":
-   	message.send('Vielleicht kommt %s mit zum Mittagessen' % getUserName, '7iqsqeqaefdmdcy4nj7ccnwfnw')
+        message.send('Vielleicht kommt %s mit zum Mittagessen' %
+                     getUserName, '7iqsqeqaefdmdcy4nj7ccnwfnw')
     else:
-        message.send('%s Kommt nicht mit zum Mittagessen' % getUserName, '7iqsqeqaefdmdcy4nj7ccnwfnw')
+        message.send(
+            '%s Kommt nicht mit zum Mittagessen' %
+            getUserName,
+            '7iqsqeqaefdmdcy4nj7ccnwfnw')
 
 mittagessen_aufruf.__doc__ = "Zeigt Speiseplan an und macht Umfrage, wer zum Mittagessen mitkommt"
 
@@ -27,22 +37,33 @@ mittagessen_aufruf.__doc__ = "Zeigt Speiseplan an und macht Umfrage, wer zum Mit
 @listen_to('MittagessenUm (.*)')
 def mittagessen_um(message, uhrzeit):
     getUserName = getpass.getuser()
-    kommtMit = pymsgbox.confirm('Kommst Du mit zum Mittagessen um %s' % uhrzeit , 'Teilnahme Mittagessen', ["Ja", "Vielleicht", 'Nein'])
+    kommtMit = pymsgbox.confirm(
+        'Kommst Du mit zum Mittagessen um %s' %
+        uhrzeit, 'Teilnahme Mittagessen', [
+            "Ja", "Vielleicht", 'Nein'])
     if kommtMit == "Ja":
-        message.send('Ja, %s kommt mit zum Mittagessen' % getUserName, '7iqsqeqaefdmdcy4nj7ccnwfnw')
+        message.send(
+            'Ja, %s kommt mit zum Mittagessen' %
+            getUserName,
+            '7iqsqeqaefdmdcy4nj7ccnwfnw')
     elif kommtMit == "Vielleicht":
-        message.send('Vielleicht kommt %s mit zum Mittagessen' % getUserName, '7iqsqeqaefdmdcy4nj7ccnwfnw')
+        message.send('Vielleicht kommt %s mit zum Mittagessen' %
+                     getUserName, '7iqsqeqaefdmdcy4nj7ccnwfnw')
     else:
-        message.send('%s Kommt nicht mit zum Mittagessen' % getUserName, '7iqsqeqaefdmdcy4nj7ccnwfnw')
+        message.send(
+            '%s Kommt nicht mit zum Mittagessen' %
+            getUserName,
+            '7iqsqeqaefdmdcy4nj7ccnwfnw')
 
 mittagessen_um.__doc__ = "Umfrage, wer zu einer bestimmten Zeit zum Mittagessen mitkommt"
+
 
 @respond_to('SpeiseplanAnzeigen')
 @listen_to('SpeiseplanAnzeigen')
 def mittagessen_speiseplan(message):
     # Wichtig, damit nur der ausfuehrende User die Antwort angezeigt bekommt
     if message.get_username() == getpass.getuser():
-	os.system('xdg-open "http://www.aserv.kit.edu/downloads/Speiseplan_deutsch.pdf"')
+        os.system(
+            'xdg-open "http://www.aserv.kit.edu/downloads/Speiseplan_deutsch.pdf"')
 
 mittagessen_speiseplan.__doc__ = u"Zeigt Speiseplan für einzelnen Benutzer an"
-
